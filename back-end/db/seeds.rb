a9c479d33d7ad498dbce79da00b960da154e3f87
Vinicius Meneses <vinicius.meneses04@gmail.com>
2023-01-12 16:30:56 -0300

    chore: seed database

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+metrics = [
+  Metric.create(name: "Signups"),
+  Metric.create(name: "New Customers"),
+  Metric.create(name: "Upsells")
+]
+
+metrics.each do |metric|
+  random_records = 1000.times.map do
+    {
+      metric_id: metric.id,
+      timestamp: Faker::Time.backward,
+      value: Faker::Number.between(from: 5, to: 500)
+    }
+  end
+
+  Record.insert_all(random_records)
+end
