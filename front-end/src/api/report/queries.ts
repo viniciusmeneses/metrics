6aa5bf863a38516421294613c29fc4e5ae959792
Vinicius Meneses <vinicius.meneses04@gmail.com>
2023-02-13 23:31:24 -0300

    refactor: api abstraction

--- front-end/src/api/report/queries.ts
+++ front-end/src/api/report/queries.ts
@@ -4,20 +4,14 @@ import { api } from "../api";
 
 import { AllMetricsReport, SingleMetricReport } from "./typings";
 
-const DEFAULT_GROUP_BY = "day";
-
-export const useAllMetricsReport = (params: AllMetricsReport.Params) => {
-  const groupBy = params.groupBy ?? DEFAULT_GROUP_BY;
-
-  return useQuery<AllMetricsReport.Success, AllMetricsReport.Error>({
+export const useAllMetricsReport = ({ groupBy }: AllMetricsReport.Params) =>
+  useQuery<AllMetricsReport.Success, AllMetricsReport.Error>({
     queryKey: ["AllMetricsReport", groupBy],
     queryFn: () => api.get("/reports/metrics", { params: { group_by: groupBy } }),
   });
-};
 
-export const useSingleMetricReport = ({ id, ...params }: SingleMetricReport.Params) => {
+export const useSingleMetricReport = ({ id, groupBy }: SingleMetricReport.Params) => {
   const queryClient = useQueryClient();
-  const groupBy = params.groupBy ?? DEFAULT_GROUP_BY;
 
   const initialData = () =>
     queryClient
--- front-end/src/testUtils.tsx
+++ front-end/src/testUtils.tsx
@@ -1,9 +1,15 @@
 import { PropsWithChildren, ReactElement } from "react";
 
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { render as rtlRender, RenderOptions } from "@testing-library/react";
 
-const AllProviders = (props: PropsWithChildren) => <ChakraProvider theme={theme} {...props} />;
+import { ApiProvider } from "./api";
+
+const AllProviders = (props: PropsWithChildren) => (
+  <ChakraProvider>
+    <ApiProvider {...props} />
+  </ChakraProvider>
+);
 
 export const render = (ui: ReactElement, options?: RenderOptions) =>
   rtlRender(ui, { wrapper: AllProviders, ...options });
